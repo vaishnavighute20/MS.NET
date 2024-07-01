db7f38e838297b7147b3d737751fd15e8441cb4a
vaishnavighute20 <vaishnavighute20@gmail.com>
2024-07-01 14:17:35 +0530

    HelloWordApp

---
+++ Day1/TestHello/TestHello/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using TestHello;
+
+Console.WriteLine("Hello, World!");
+//Person person = new Person();
+//person.setName("ravi");
+Employee employee = new Employee(122,"Vaishu",29,20000,1000);
+Console.WriteLine(employee);
